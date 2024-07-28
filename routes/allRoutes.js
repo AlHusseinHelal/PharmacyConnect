02966a97ffc87b8996892c45a56c57b25d44dcf7
AlHusseinHelal <dhs2010.hh@gmail.com>
2024-07-28 08:56:00 +0300

    PharmacyConnect

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -3396,9 +3396,9 @@ router.post(
       return res.json({ passwordnotmatch: "Password Not Match" });
     }
 
-    if (!email.includes("@57357.org")) {
-      return res.json({ invalidemail: "Invalid Email"})
-    }
+    // if (!email.includes("@57357.org")) {
+    //   return res.json({ invalidemail: "Invalid Email"})
+    // }
 
 
     const newUser = await User.create(req.body);
